4cac4720a2db09258ea5a2c1cc1aba2e14861b51
mart-gant <martgant@gmail.com>
2020-11-08 17:59:51 +0100

    Final commit

---
+++ BMI-calculator/BMI-calculator/BMI-calculator/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  BMI-calculator
+//
+//  Created by user917318 on 3/17/20.
+//  Copyright © 2020 user917318. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BMI-calculator/BMI-calculator/BMI-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-calculator
+//
+//  Created by user917318 on 3/17/20.
+//  Copyright © 2020 user917318. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
